6188f00f70b8e64492176148aa85266db392ac92
pavloteyfel <pavloteyfel@gmail.com>
2021-12-16 21:53:18 +0100

    refactor

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,27 +1,26 @@
-def pivot(array, start, end):
-    pivot_value = array[start] # the pivot value always the first number
-    swap_index = start
-
-    for i in range(start + 1, end + 1): # we can omit the first number as it is the pivot
-        if array[i] < pivot_value:
-            swap_index += 1
-            swap(array, swap_index, i)
-
-    swap(array, swap_index, start)
-
-    return swap_index    
-
-def swap(array, i, j):
+def swap(array, i, j ):
     array[i], array[j] = array[j], array[i]
-    return array
+
+def pivot(array, start, end):
+    pivot_index = start
+    swap_index = start
+    for i in range(start + 1, end + 1):
+        if array[i] < array[pivot_index]:
+            swap_index += 1
+            swap(array, i, swap_index)
+    swap(array, pivot_index, swap_index)
+    return swap_index
 
 def quick_sort(array):
-    last_index = len(array) - 1
-    def _sort(array, left=0, right=last_index): # to be consistent with the left, start, right, end values
+    def sort(array, left, right):
         if left < right:
             pivot_index = pivot(array, left, right)
-            _sort(array, left, pivot_index - 1)
-            _sort(array, pivot_index + 1, right)
+            sort(array, left, pivot_index - 1)
+            sort(array, pivot_index + 1, right)
         return array
-    return _sort(array)
+    return sort(array, 0, len(array) - 1)
 
+
+assert quick_sort([4, 6, 3, 1, 2, 8, 9]) == [1, 2, 3, 4, 6, 8, 9]
+assert quick_sort([1]) == [1]
+assert quick_sort([]) == []
