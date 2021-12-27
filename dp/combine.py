1e59e007b9878f409fb5f2b9da66484d0b02c876
pavloteyfel <pavloteyfel@gmail.com>
2021-12-27 19:47:33 +0100

    feature: Add combine function

---
+++ dp/combine.py
@@ -0,0 +1,16 @@
+def combine(array, k):
+    global counter
+
+    if k == 0:
+        return [[]]
+
+    inner_list = []
+
+    for i in range(0, len(array)):
+        first = array[i]
+        sub_result = combine(array[i+1:], k-1)
+        for result in sub_result:
+            inner_list.append([first] + result)
+    return inner_list
+
+
