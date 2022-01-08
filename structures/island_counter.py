7d6d547c23778cb6f6344e03e95d5881590e39bf
pavloteyfel <pavloteyfel@gmail.com>
2022-01-08 21:51:14 +0100

    style: Add style

--- structures/island_counter.py
+++ structures/island_counter.py
@@ -8,24 +8,26 @@ graph = {
     "e": ["d", "c"],
 }
 
+
 def shortest_path(graph, src, dst):
     queue = deque()
     memo = set()
 
     queue.append((src, 0))
     memo.add(src)
-    
+
     while queue:
         node, distance = queue.popleft()
 
         if node == dst:
             return distance
-        
+
         for neighbour in graph[node]:
             if neighbour not in memo:
                 queue.append((neighbour, distance + 1))
                 memo.add(neighbour)
-    
+
     return -1
+
 
 print(shortest_path(graph, "a", "c"))
