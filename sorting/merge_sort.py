539a7ddb5784a0df2746ab6a8a5772b1988eb232
pavloteyfel <pavloteyfel@gmail.com>
2022-01-16 22:16:46 +0100

    style: Change style

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -11,8 +11,10 @@ def merge(array1, array2):
     result = sorted(array1 + array2)
     return result
 
-# One liner for fun
 
-
-def sort(arr):
-    return arr if (l := len(arr)) == 1 else sorted(sort(arr[:l//2]) + sort(arr[l//2:]))
+def merge_sort_v2(arr):
+    return (
+        arr
+        if (l := len(arr)) == 1
+        else sorted(merge_sort_v2(arr[: l // 2]) + merge_sort_v2(arr[l // 2 :]))
+    )
