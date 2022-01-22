88f21b1fc1d543b6fc5e353cbeac055cc52736e8
pavloteyfel <pavloteyfel@gmail.com>
2022-01-22 23:04:28 +0100

    style: Change styling

--- dp/filter_odd.py
+++ dp/filter_odd.py
@@ -10,18 +10,18 @@ def filter_odd(array):
     # if not array: ...
     if len(array) == 0:
         return containter
-    
+
     # Check if the first number is odd, and put it into container
     if array[0] % 2 != 0:
         containter.append(array[0])
-    
+
     # Python can handle [1:] on an empy list, but I added this check to spare
     # one more unnecessary recursion call
     if len(array) == 1:
         return containter
 
     # In this phase we probably have something in our container, an odd number
-    # or an empy list. Let's concatenate it with the result of the result of 
+    # or an empy list. Let's concatenate it with the result of the result of
     # the remaining filtering
     return containter + filter_odd(array[1:])
 
@@ -29,8 +29,7 @@ def filter_odd(array):
 import math
 
 
-
-def p(n,m):
+def p(n, m):
     if m == 0:
         return 1
 
@@ -38,13 +37,13 @@ def p(n,m):
         return n
 
     if m == -1:
-        return 1/n
-    
+        return 1 / n
+
     if m < 0:
-        return 1/n * p(n, m+1)
+        return 1 / n * p(n, m + 1)
 
-    return n * p(n, m-1)
+    return n * p(n, m - 1)
 
-print(pow(3,-4))
-print(p(3,-4))
 
+print(pow(3, -4))
+print(p(3, -4))
