cb3234427a9e04cd792074eb1e88752014169f8b
pavloteyfel <pavloteyfel@gmail.com>
2022-01-29 23:01:48 +0100

    feature: Add solution

--- problems/max_path_sum.py
+++ problems/max_path_sum.py
@@ -2,11 +2,13 @@
 number values. The function should return the maximum sum of any root to leaf path 
 within the tree."""
 
+
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 a = Node(3)
 b = Node(11)
@@ -20,3 +22,16 @@ a.right = c
 b.left = d
 b.right = e
 c.right = f
+
+
+def max_path_sum(root):
+    if root is None:
+        return float("-inf")
+
+    if root.left is None and root.right is None:
+        return root.value
+
+    return root.value + max(max_path_sum(root.left), max_path_sum(root.right))
+
+
+assert max_path_sum(a) == 18
