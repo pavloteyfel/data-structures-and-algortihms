158887b8a916b16efff5efcfec3368d5020ca549
pavloteyfel <pavloteyfel@gmail.com>
2022-01-29 22:54:23 +0100

    feature: Add solution

--- problems/tree_sum.py
+++ problems/tree_sum.py
@@ -1,11 +1,13 @@
 """Write a function, tree_sum, that takes in the root of a binary tree that contains 
 number values. The function should return the total sum of all values in the tree."""
 
+
 class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 a = Node(3)
 b = Node(11)
@@ -20,3 +22,11 @@ b.left = d
 b.right = e
 c.right = f
 
+
+def tree_sum(root):
+    if not root:
+        return 0
+    return root.value + tree_sum(root.left) + tree_sum(root.right)
+
+
+assert (tree_sum(a)) == 21
